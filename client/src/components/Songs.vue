b9cd8af925e449004e8112bc43e2a44a996a6434
hoa le <lehoanghoa1008@gmail.com>
2018-04-05 22:24:44 +0900

    Part 4 - commit 1

--- client/src/components/Songs.vue
+++ client/src/components/Songs.vue
@@ -2,17 +2,50 @@
    <v-layout row wrap>
     <v-flex xs6 offset-xs3>
       <panel title="Songs">
+        <v-btn
+          slot="action"
+          @click="navigateTo({name: 'songs-create'})"
+          class="cyan accent-2"
+          light
+          medium
+          absolute
+          right
+          middle
+          fab>
+          <v-icon>add</v-icon>
+        </v-btn>
+        <div
+          v-for="song in songs"
+          :key="song.id">
+          {{song.title}} -
+          {{song.artist}} -
+          {{song.album}}
+        </div>
       </panel>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
+import SongsService from '@/services/SongsService'
 import Panel from '@/components/Panel'
 
 export default {
   components: {
     Panel
+  },
+  data () {
+    return {
+      songs: null
+    }
+  },
+  methods: {
+    navigateTo (route) {
+      this.$router.push(route)
+    }
+  },
+  async mounted () {
+    this.songs = (await SongsService.index()).data
   }
 }
 </script>
